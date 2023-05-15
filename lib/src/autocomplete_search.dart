9b90aa2bd35d09597b480abd49f312d078aa0872
alfacentaury <alfa.arnialfa@gmail.com>
2023-05-15 11:16:35 +0700

    Adjust code autocomplete search

--- lib/src/autocomplete_search.dart
+++ lib/src/autocomplete_search.dart
@@ -303,20 +303,25 @@ class AutoCompleteSearchState extends State<AutoCompleteSearch> {
   }
 
   Widget _buildPredictionOverlay(List<Prediction> predictions) {
-    return ListView.builder(
-      physics: const FixedExtentScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: predictions.length,
-      shrinkWrap: true,
-      itemBuilder: (_, index) {
-        return PredictionTile(
-          prediction: predictions[index],
-          onTap: (selectedPrediction) {
-            resetSearchBar();
-            widget.onPicked(selectedPrediction);
+    return Container(
+      color: Colors.white,
+      child: Flexible(
+        child: ListView.builder(
+          physics: const FixedExtentScrollPhysics(),
+          padding: EdgeInsets.zero,
+          itemCount: predictions.length,
+          shrinkWrap: true,
+          itemBuilder: (_, index) {
+            return PredictionTile(
+              prediction: predictions[index],
+              onTap: (selectedPrediction) {
+                resetSearchBar();
+                widget.onPicked(selectedPrediction);
+              },
+            );
           },
-        );
-      },
+        ),
+      ),
     );
   }
 
